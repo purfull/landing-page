b96f81b423e278b0029d803ef2b4304d8ffdd32d
DevaprakashP <devaprakashdearson@gmail.com>
2025-04-29 14:48:56 +0530

    changes uploaded

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,8 @@ import NavBarComp from "./components/Navbar.jsx";
 import Menu from "./components/Menu/menu.jsx";
 import Footer from "./components/Footer/footer.jsx";
 import Testimonial from "./components/Testimonial/test.jsx";
-import NavBarComp from "./components/Navbar.jsx";
-import Menu from "./components/Menu/menu.jsx";
-import Footer from "./components/Footer/footer.jsx";
 import Calledtoaction from "./components/cta/calledtoactiongrid.jsx";
 import LocationAndOrder from "./components/locationandorder/locationandorder.jsx";
-import Testimonial from "./components/Testimonial/test.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
